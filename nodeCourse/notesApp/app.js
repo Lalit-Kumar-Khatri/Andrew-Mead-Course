b9d360f12a85dbdbb7b5d3fcc621de93f1bc7db6
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-02-27 14:54:21 +0530

    Completed the challenge based on NPM packages

--- nodeCourse/notesApp/app.js
+++ nodeCourse/notesApp/app.js
@@ -1,10 +1,22 @@
-const validator = require('validator');
-
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 const myNotes = getNotes();
 console.log(myNotes);
 
-console.log(validator.isEmail('example.com'));
+const greenMsg = chalk.green('Success!');
 
-console.log(validator.isURL('https/mead.io'));
+console.log(greenMsg);
+
+console.log(chalk.bold.green('Success!'));
+
+console.log(chalk.inverse.green('Success!'));
+
+
+// Challenge
+// 1. Install version 2.4.1 of chalk
+// 2. Load chalk into app.js
+// 3. Use it to print the string "Success!" to the console in green
+// 4. Test your work
+
+// Bonus: Use docs to mess around with other styles. Make text bold and inversed
