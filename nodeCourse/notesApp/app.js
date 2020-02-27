b56f6f931512739e550451fb1a491a8f0ae16445
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-02-27 15:11:41 +0530

    Installed nodemon global npm package

--- nodeCourse/notesApp/app.js
+++ nodeCourse/notesApp/app.js
@@ -4,19 +4,5 @@ const getNotes = require('./notes');
 const myNotes = getNotes();
 console.log(myNotes);
 
-const greenMsg = chalk.green('Success!');
-
+const greenMsg = chalk.blue.inverse.bold('Error!');
 console.log(greenMsg);
-
-console.log(chalk.bold.green('Success!'));
-
-console.log(chalk.inverse.green('Success!'));
-
-
-// Challenge
-// 1. Install version 2.4.1 of chalk
-// 2. Load chalk into app.js
-// 3. Use it to print the string "Success!" to the console in green
-// 4. Test your work
-
-// Bonus: Use docs to mess around with other styles. Make text bold and inversed
