41fa9da3411bf37e9183e166365a31195c77bd7a
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-02-28 14:00:08 +0530

    Learned about setting up command options

--- nodeCourse/notesApp/app.js
+++ nodeCourse/notesApp/app.js
@@ -5,12 +5,31 @@ const getNotes = require('./notes');
 // Customize yargs version
 yargs.version('1.1.0');
 
+// Challenge:
+// 1. Setup a body option for the add command
+// 2. Configure a description, make it required, and for it to be a string
+// 3. Log the body value in the handler function
+// 4. Test your work
+
 // Create add command
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () {
-        console.log('Adding a new note');
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
     }
 })
 
@@ -23,12 +42,6 @@ yargs.command({
     }
 })
 
-// Challenge:
-// 1. Setup command to support 'list' command (print placeholder message for now)
-// 2. Setup command to support 'read' command (print placeholder message for now)
-// 3. Test your work by running both commands and ensure correct output
-
-// Create list command
 yargs.command({
     command: 'list',
     describe: 'List the notes',
@@ -46,4 +59,4 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv);
+yargs.parse();
