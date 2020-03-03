6ab58a2a6c058ade62fd9e1d40b7fd8a945fbeda
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-03-03 13:14:20 +0530

    Completed all three challenges based on removing a note

--- nodeCourse/notesApp/notes.js
+++ nodeCourse/notesApp/notes.js
@@ -1,43 +1,59 @@
-const fs = require('fs');
+const fs = require('fs')
+const chalk = require('chalk')
 
 const getNotes = function () {
-    return "Your notes...";
+    return 'Your notes...'
 }
 
 const addNote = function (title, body) {
-    const notes = loadNotes();
+    const notes = loadNotes()
     const duplicateNotes = notes.filter(function (note) {
         return note.title === title
     })
 
     if (duplicateNotes.length === 0) {
         notes.push({
-        title: title,
-        body: body
+            title: title,
+            body: body
         })
-        saveNotes(notes);
-        console.log('New note added!')
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
     } else {
-        console.log('Note title taken!')
+        console.log(chalk.red.inverse('Note title taken!'))
     }
 }
 
+const removeNote = function (title) {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title
+    })
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
+    }    
+}
+
 const saveNotes = function (notes) {
-    const dataJSON = JSON.stringify(notes);
+    const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
 const loadNotes = function () {
     try {
-        const dataBuffer = fs.readFileSync('notes.json');
-        const dataJSON = dataBuffer.toString();
-        return JSON.parse(dataJSON);
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
     } catch (e) {
-        return []   
+        return []
     }
 }
 
 module.exports = {
     getNotes: getNotes,
-    addNote: addNote
+    addNote: addNote,
+    removeNote: removeNote
 }
