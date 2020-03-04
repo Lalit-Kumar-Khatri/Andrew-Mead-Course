9b911adba2b9129e006633209712e4b4cf805456
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-03-04 13:26:28 +0530

    Completed the challenge based on reading a note

--- nodeCourse/notesApp/notes.js
+++ nodeCourse/notesApp/notes.js
@@ -1,15 +1,23 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = () => {
-    return 'Your notes...'
-}
+// 
+// Goal: Wire up read command
+// 
+// 1. Setup --title option for read command
+// 2. Create readNote in note.js
+// - search for note by title
+// - find note and print title (styled) and body (plain)
+// - no note found? Print error in red
+// 3. Have the command handler call the function
+// 4. Test your command by running a couple commands
+
 
 const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter((note)=> note.title === title)
+    const duplicateNote = notes.find((note)=> note.title === title);
 
-    if (duplicateNotes.length === 0) {
+    if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
@@ -42,6 +50,20 @@ const listNotes = () => {
     })
 }
 
+const readNote = (title) => {
+    const notes = loadNotes();
+    const note = notes.find((note) => note.title === title);
+
+    if (note) {
+        console.log(chalk.inverse(note.title));
+        console.log(note.body);
+    } else {
+        console.log(chalk.red.inverse('Note not found'));
+    }
+
+}
+
+
 const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
@@ -58,8 +80,8 @@ const loadNotes = () => {
 }
 
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 }
