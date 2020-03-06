bfa42eba3950d48d00a75770eecaad47ecc61a2b
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-03-06 22:57:40 +0530

    Explored a little more request module and darksky API, and completed the challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,13 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/d08c05a51ac0554fc0553a9cfe47524e/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/d08c05a51ac0554fc0553a9cfe47524e/37.8267,-122.4233?lang=hi'
 
-request({ url: url }, (error, response)=> {
-    const data = JSON.parse(response.body);
-    console.log(data.currently);
+request({ url: url, json: true }, (error, response)=> {
+        // console.log(response.body.currently);
+        console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 });
+
+// Goal: Print a small forecast to the user
+// 
+// 1. Print: "It is currently 58.55 degrees out. There is a 0% chance of rain."
+// 2. Test your work.
