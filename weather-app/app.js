353ac53d30364985e989cda266c26563997a8a7e
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-03-10 18:50:04 +0530

    Learned about geocoding and integrated to our app and completed the challenge based on above

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,7 +7,23 @@ request({ url: url, json: true }, (error, response)=> {
         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 });
 
-// Goal: Print a small forecast to the user
+
+// Geocoding
+// Address -> Lat/Long -> Weather
+
+
 // 
-// 1. Print: "It is currently 58.55 degrees out. There is a 0% chance of rain."
-// 2. Test your work.
+// Goal: Print the lat/long for los Angeles
+// 
+// 1. Fire off a new request to the URL explored in the browser
+// 2. Have the request module parse it as JSON
+// 3. Print both the latitude and longtitude to the terminal
+// 4. Test your work! 
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGFsc2EiLCJhIjoiY2s3bHZ4ZnFpMDdscDNmcGF1MXl2dGs2dSJ9.eTfuEtnSRT_2IcA4t3DiOg&limit=1';
+
+request({ url: geocodeURL, json: true}, (error, response) => {
+        const latitude = response.body.features[0].center[1];
+        const longtitude = response.body.features[0].center[0];
+        console.log(latitude, longtitude);
+});
