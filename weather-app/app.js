8dbacf82e5020ed3868fc0b1b1274fc0cb8563b5
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-03-13 16:12:29 +0530

    Learned about error handling and completed the challenge in respect of that too

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,29 +1,36 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/d08c05a51ac0554fc0553a9cfe47524e/37.8267,-122.4233?lang=hi'
+// const url = 'https://api.darksky.net/forecast/d08c05a51ac0554fc0553a9cfe47524e/37.8267, -112'
 
-request({ url: url, json: true }, (error, response)=> {
-        // console.log(response.body.currently);
-        console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
-});
-
-
-// Geocoding
-// Address -> Lat/Long -> Weather
-
+// request({ url: url, json: true }, (error, response)=> {
+//         if (error) {
+//                 console.log('Unable to connect to weather service!');
+//         } else if (response.body.error) {
+//                 console.log('Unable to find location')
+//         } else {
+//                 console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' +    response.body.currently.precipProbability + '% chance of rain.');
+//         }
+// });
+        
 
 // 
-// Goal: Print the lat/long for los Angeles
+// Goal: Handle error geocoding request
 // 
-// 1. Fire off a new request to the URL explored in the browser
-// 2. Have the request module parse it as JSON
-// 3. Print both the latitude and longtitude to the terminal
-// 4. Test your work! 
+// 1. Setup an error handler for low-level errors.
+// 2. Test by disabling the network request and running the app
+// 3. Setup error handling for no matching results.
+// 4. Test by altering the search term and running the app 
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGFsc2EiLCJhIjoiY2s3bHZ4ZnFpMDdscDNmcGF1MXl2dGs2dSJ9.eTfuEtnSRT_2IcA4t3DiOg&limit=1';
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoibGFsc2EiLCJhIjoiY2s3bHZ4ZnFpMDdscDNmcGF1MXl2dGs2dSJ9.eTfuEtnSRT_2IcA4t3DiOg&limit=1'
 
-request({ url: geocodeURL, json: true}, (error, response) => {
-        const latitude = response.body.features[0].center[1];
-        const longtitude = response.body.features[0].center[0];
-        console.log(latitude, longtitude);
-});
+request({ url: geocodeURL, json: true }, (error, response) => {
+        if (error) {
+                console.log('Unable to connect to location services!')
+        } else if (response.body.features.length === 0) {
+                console.log('Unable to find location. Try another search.');
+        } else {
+                const longtitude = response.body.features[0].center[1]
+                const latitude = response.body.features[0].center[0]
+                console.log(longtitude, latitude);
+        }
+})
