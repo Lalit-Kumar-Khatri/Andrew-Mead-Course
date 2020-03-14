e8062f1c0f8590463611e0eeb5d1601341d9fe30
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-03-14 22:25:57 +0530

    Learned about callback abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const geocode = require('./utils/geocode');
 
 // const url = 'https://api.darksky.net/forecast/d08c05a51ac0554fc0553a9cfe47524e/37.8267, -112'
 
@@ -8,21 +9,27 @@ const request = require('request');
 //         } else if (response.body.error) {
 //                 console.log('Unable to find location')
 //         } else {
-//                 console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' +    response.body.currently.precipProbability + '% chance of rain.');
+//                exports.mo console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' +    response.body.currently.precipProbability + '% chance of rain.');
 //         }
 // });
         
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoibGFsc2EiLCJhIjoiY2s3bHZ4ZnFpMDdscDNmcGF1MXl2dGs2dSJ9.eTfuEtnSRT_2IcA4t3DiOg&limit=1'
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoibGFsc2EiLCJhIjoiY2s3bHZ4ZnFpMDdscDNmcGF1MXl2dGs2dSJ9.eTfuEtnSRT_2IcA4t3DiOg&limit=1'
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-        if (error) {
-                console.log('Unable to connect to location services!')
-        } else if (response.body.features.length === 0) {
-                console.log('Unable to find location. Try another search.');
-        } else {
-                const longtitude = response.body.features[0].center[1]
-                const latitude = response.body.features[0].center[0]
-                console.log(longtitude, latitude);
-        }
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//         if (error) {
+//                 console.log('Unable to connect to location services!')
+//         } else if (response.body.features.length === 0) {
+//                 console.log('Unable to find location. Try another search.');
+//         } else {
+//                 const longtitude = response.body.features[0].center[1]
+//                 const latitude = response.body.features[0].center[0]
+//                 console.log(longtitude, latitude);
+//         }exports.mo
+// })
+
+
+geocode('Boston', (error, data) => {
+        console.log('Error', error);
+        console.log('Data', data);
 })
