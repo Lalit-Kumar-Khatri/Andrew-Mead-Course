4a656b8ffdb812682b9cf9f8300f5f371e8f2bef
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-03-16 15:08:52 +0530

    Learned about callback chaining and completed the challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,38 +1,32 @@
-const request = require('request');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-// const url = 'https://api.darksky.net/forecast/d08c05a51ac0554fc0553a9cfe47524e/37.8267, -112'
+const address = process.argv[2];
 
-// request({ url: url, json: true }, (error, response)=> {
-//         if (error) {
-//                 console.log('Unable to connect to weather service!');
-//         } else if (response.body.error) {
-//                 console.log('Unable to find location')
-//         } else {
-//                exports.mo console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' +    response.body.currently.precipProbability + '% chance of rain.');
-//         }
-// });
-
-
-// geocode('Boston', (error, data) => {
-//         console.log('Errarrayor', error);
-//         console.log('Data', data);
-// })
-
-
-
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-forecast(-75.7088, 44.1545, (error, data) => {
-        console.log('Error', error)
-        console.log('Data', data)
+if (!address) {
+  console.log('Please provide an address');
+} else {
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+    
+      forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+          return console.log(error);
+        }
+          console.log(data.location);
+          console.log(forecastData);
       })
+    })  
+}
+
+// 
+// Goal: Accept location via command line argument
+// 
+// 1. Access the command line without yargs
+// 2. Use the string value as the input for geocode
+// 3. Only geocode if the location was provided
+// 4. Test your work with couple locations
+// 
+
