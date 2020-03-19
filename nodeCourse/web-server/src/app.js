2aae976dcce9c79d1247a84fda4e70b004c5305c
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-03-20 00:24:05 +0530

    Learned advanced templating as partials

--- nodeCourse/web-server/src/app.js
+++ nodeCourse/web-server/src/app.js
@@ -1,16 +1,26 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+
+// 
+// Goal: Create a partial for the footer
+// 
+// 1. Setup the template for the footer partial "Created By Some Name"
+// 2. Render the partial at the bottom of all three pages
+// 3. Test your work by visiting all three pages
 
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -33,7 +43,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.'
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
     })
 })
 
