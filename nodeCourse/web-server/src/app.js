00a068a2597626d6bb36aeab559814fa0ea0036e
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-03-19 23:21:18 +0530

    Learned about handlebars for dynamic contents

--- nodeCourse/web-server/src/app.js
+++ nodeCourse/web-server/src/app.js
@@ -5,13 +5,37 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
+app.get('', (req, res)  => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
+    });
 })
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Andrew Mead'
+    })
+})
+
+
+// 
+// Goal: Create a template for help page
+// 
+// 1. Setup a help template to render a help message to the screen
+// 2. Setup the help route and render the template with an example message
+// 3. Visit the route in the browser and see your help message print
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
