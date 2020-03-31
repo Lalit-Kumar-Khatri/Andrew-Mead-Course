e27acf236dcd49ad8b0dbdb62b7c866524df4d8c
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-03-31 13:20:36 +0530

    Learned about reading the data from database and used their methods; find() and findOne() and completed the challenge based on such

--- nodeCourse/task-manager/mongodb.js
+++ nodeCourse/task-manager/mongodb.js
@@ -5,9 +5,6 @@ const { MongoClient, ObjectID } = require('mongodb');
 const connectioinURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-const id = new ObjectID();
-console.log(id.id.length);
-console.log(id.toHexString().length);
 
 
 MongoClient.connect(connectioinURL, { useNewUrlParser: true }, (error, client) => {
@@ -17,57 +14,34 @@ MongoClient.connect(connectioinURL, { useNewUrlParser: true }, (error, client) =
 
     const db = client.db(databaseName);
 
-    // db.collection('users').insertOne({
-    //     name: 'Vikram',
-    //     age: 26
-    // }, (error, result) => {
+    // db.collection('users').findOne({ _id: new ObjectID("5e82e6227514843cadbcf8b9") }, (error, user) => {
     //     if (error) {
-    //         return console.log('Unable to insert user')
+    //         return console.log('Unable to fetch');
     //     }
 
-    //     console.log(result.ops)
+    //     console.log(user);
     // })
 
-//     db.collection('users').insertMany([
-//         {
-//             name: 'Jen',
-//             age: 28
-//         }, {
-//             name: 'Gunther',
-//             age: 27
-//         }
-//     ], (error, result) => {
-//         if (error) {
-//             return console.log('Unable to insert documents!');
-//         }
+    // db.collection('users').find({ age: 27 }).toArray((error, users) => {
+    //     console.log(users);
+    // })
 
-//         console.log(result.ops);
-//     })
+    // db.collection('users').find({ age: 27 }).count((error, count) => {
+    //     console.log(count);
+    // })
 
+    // 
+    // Goal: Use find and findOne with tasks
+    // 
+    // 1. Use findOne to fetch the last task by its id (print doc to the console)
+    // 2. Use find to fetch all the tasks that are not completed (print docs to the console)
+    // 3. Test your work!
     
+    db.collection('tasks').findOne({ _id: new ObjectID("5e82dd37ae2d773658298693") }, (error, task) => {
+        console.log(task);
+    })
 
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Clean the house',
-    //         completed: true
-    //     },
-        
-    //     {
-    //         description: 'Renew inspection',
-    //         completed: false
-    //     },
-       
-    //     {
-    //         description: 'Pot plants',
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-
-    //     if (error) {
-    //         return console.log('Unable to insert tasks!');
-    //     }
-
-    //     console.log(result.ops);
-    // })
-
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks);
+    })
 })
