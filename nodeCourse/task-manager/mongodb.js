3354026d178494cf7d43ee0b5313e71cdee78bf1
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-04-01 08:41:34 +0530

    Setup mongoose npm package and explored it a litte

--- nodeCourse/task-manager/mongodb.js
+++ nodeCourse/task-manager/mongodb.js
@@ -14,21 +14,13 @@ MongoClient.connect(connectioinURL, { useNewUrlParser: true }, (error, client) =
 
     const db = client.db(databaseName);
 
-    db.collection('users').deleteMany({
-        age: 27
-    }).then((result) => {
-        console.log(result);
-    }).catch((error) => {
-        console.log(error);
-    })
-
-    
-    // Goal: Use deleteOne to remove a task
-    
-    // 1. Grab the description for the task you want to remove
-    // 2. Set up the call with the query
-    // 3. Use promise method to set up the success/error handlers
-    // 4. Test your work.
+    // db.collection('users').deleteMany({
+    //     age: 27
+    // }).then((result) => {
+    //     console.log(result);
+    // }).catch((error) => {
+    //     console.log(error);
+    // })
 
     db.collection('tasks').deleteOne({
         description: "Clean the house"
