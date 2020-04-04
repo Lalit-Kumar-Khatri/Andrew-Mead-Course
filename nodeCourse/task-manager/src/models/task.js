34e0421faf0adb661e9b5fb385eb6e9443cd5832
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-04-04 08:09:33 +0530

    Completed the challenge based on promise chaining

--- nodeCourse/task-manager/src/models/task.js
+++ nodeCourse/task-manager/src/models/task.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const Task = mongoose.model('task', {
+const Task = mongoose.model('Task', {
     description: {
         type: String,
         required: true,
@@ -8,8 +8,8 @@ const Task = mongoose.model('task', {
     },
     completed: {
         type: Boolean,
-        defalut: false
+        default: false
     }
 })
 
-module.exports = Task;
+module.exports = Task
