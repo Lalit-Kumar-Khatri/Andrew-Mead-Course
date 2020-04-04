039a38ebe69d7b432a94167bd5e34a806cfc3a5a
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-04-04 09:47:25 +0530

    Implemented Async/Await to our codes and completed the challenge

--- nodeCourse/task-manager/playground/promise-chaining-2.js
+++ nodeCourse/task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,34 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5e85c08974738345da71ea7e').then((task) => {
-    console.log(task);
-    return Task.countDocuments({ complete: false })
-}).then((result) => {
-    console.log(result);
+// Task.findByIdAndDelete('5e85c08974738345da71ea7e').then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ complete: false })
+// }).then((result) => {
+//     console.log(result);
+// }).catch((e) => {
+//     console.log(e);
+// })
+
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false });
+    return count; 
+}
+
+deleteTaskAndCount('5e85c12f69bc9046f89b5785').then((count) => {
+    console.log(count);
 }).catch((e) => {
     console.log(e);
 })
+
+// 
+// Goal: Use async/await
+// 
+// 1. Create deleteTaskAndCount as an async function
+// - accept id of task to remove
+// 2. Use await to delete task and count up incomplete tasks
+// 3. Return the count
+// 4. Call the function and attach then/catch to log results
+// 5. Test your work!
