94bc63f21a1f596208981c55d2be665093daac0c
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-04-04 08:52:43 +0530

    Learned about AsyncAwait

--- nodeCourse/task-manager/playground/promise-chaining-2.js
+++ nodeCourse/task-manager/playground/promise-chaining-2.js
@@ -1,12 +1,3 @@
-// 
-// Goal: Mess around with promise chaining
-// 
-// 1. Create promise-chaining-2.js
-// 2. Load in mongoose and task model
-// 3. Remove a given task by id
-// 4. Get and print the total number of incomplete tasks
-// 5. Test your work.
-
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
