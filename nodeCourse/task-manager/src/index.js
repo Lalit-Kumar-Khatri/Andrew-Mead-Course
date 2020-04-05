cf1e35e8f2522831408e34217bba48252fecb809
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-04-05 18:56:17 +0530

    Learned about updating endpoint with patch

--- nodeCourse/task-manager/src/index.js
+++ nodeCourse/task-manager/src/index.js
@@ -45,11 +45,28 @@ app.get('/users/:id', async (req, res) => {
 })
 
 
-// 
-// Goal: Refactor task routes to use async/await
-// 
-// 1. Refactor task routes to use async/await
-// 2. Test all routes in postman
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+})
+
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
