e925e96a6ca13ec2bcfe91a5ea7aafa023062865
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-04-12 18:38:29 +0530

    Learned about express middleware and completed the challenge based on such

--- nodeCourse/task-manager/src/index.js
+++ nodeCourse/task-manager/src/index.js
@@ -6,9 +6,29 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled');
+//     } else {
+//         next()
+//     }
+// })
+
+
+// Goal: Setup middleware for maintenance mode
+// 
+// 1. Register a new middleware function
+// 2. Send back a maintenance message with a 503 status code
+// 3. Try your requests from the server and confirm status/message shows
+
+app.use((req, res, next) => {
+    res.status(503).send('Site is currently down. Check back soon!')
+})
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
 
 
 app.listen(port, () => {
