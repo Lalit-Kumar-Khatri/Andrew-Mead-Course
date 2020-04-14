51d4ed839f141234e1869ac81ca565610ca9e87f
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-04-14 13:06:18 +0530

    Learned about creating relationship between user and task

--- nodeCourse/task-manager/src/routers/user.js
+++ nodeCourse/task-manager/src/routers/user.js
@@ -53,12 +53,6 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 })
 
-// Goal: Refactor the update profile route
-// 
-// 1. Update the URL to users/me
-// 2. Add the authentiction middleware into the mix
-// 3. Use the existing user document instead of fetching via param id
-// 4. Test your work in Postman!
 
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
