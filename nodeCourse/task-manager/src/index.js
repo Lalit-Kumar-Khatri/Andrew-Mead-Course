449b8c14cf35462ecd196483c39da7fefb7fb2b4
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-04-16 08:31:29 +0530

    Learned about filtering data through query string

--- nodeCourse/task-manager/src/index.js
+++ nodeCourse/task-manager/src/index.js
@@ -36,9 +36,9 @@ const main = async () => {
     // await task.populate('owner').execPopulate()
     // console.log(task.owner);
 
-    const user = await User.findById('5e955f8a4121f01d11e22425');
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
+//     const user = await User.findById('5e955f8a4121f01d11e22425');
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
 }
 
 main()
