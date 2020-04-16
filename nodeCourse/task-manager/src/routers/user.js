306dab01d338f1f8f23afdd048d55eac7f867027
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-04-16 10:18:29 +0530

    Learned about file uploading via npm package Multer

--- nodeCourse/task-manager/src/routers/user.js
+++ nodeCourse/task-manager/src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
@@ -81,6 +82,22 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
+const upload = multer({
+    dest: 'avatar'
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send();
+})
+
+// 
+// Goal: Setup endpoint for avatar upload
+// 
+// 1. Add POST /users/me/avatar to user router
+// 2. Setup multer to store upload in an avatars directory
+// 3. Choose name "avatar" for the key when registering the middleware
+// 4. Send back a 200 response from route handler
+// 5. Test your work, Create new Task app request and upload image
 
 
 module.exports = router;
