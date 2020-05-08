194886f051969a59aa258128a29a779e1d1e3171
lalit.khatri83@yahoo.co.in <lalit.khatri83@yahoo.co.in>
2020-05-08 13:11:20 +0530

    Learned testing for asynchronous code

--- nodeCourse/task-manager/src/math.js
+++ nodeCourse/task-manager/src/math.js
@@ -8,9 +8,22 @@ const celsiusToFahrenheit = (temp) => {
     return (temp * 1.8) + 32
 }
 
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0) {
+                return reject('Numbers must be non-negativve')
+            }
+
+            resolve(a + b);
+        }, 2000)
+    })
+}
+
 
 module.exports = {
     calculateTip,
     fahrenheitToCelsius,
-    celsiusToFahrenheit
+    celsiusToFahrenheit,
+    add
 }
